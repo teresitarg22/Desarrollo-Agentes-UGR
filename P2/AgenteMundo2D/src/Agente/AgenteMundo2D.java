672c59159bd36e0b60b686bff4243ac431cb2723
Diego Velázquez <diegovelazquezortuno.personal@gmail.com>
2023-11-12 20:15:07 +0100

    continuación agente

--- P2/AgenteMundo2D/src/Agente/AgenteMundo2D.java
+++ P2/AgenteMundo2D/src/Agente/AgenteMundo2D.java
@@ -1,22 +1,34 @@
 
 package Agente;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.AbstractMap.SimpleEntry;
+
 import jade.core.Agent;
 
 import Comportamientos.Vision;
 import Comportamientos.Mover;
 import Comportamientos.TomaDecision;
+import Comportamientos.IniciarEstado;
 
 /**
  *
  * @author Diego Velazquez Ortuño
  */
 public class AgenteMundo2D extends Agent{
+    
+    Map< SimpleEntry<Integer,Integer> , Integer > pesos = HashMap<>();
 
     // -------------------------------------------------
     // Inicialización del agente.
     @Override
     public void setup(){
-        
+        addBehaviour( new IniciarEstado() );
+        addBehaviour( new Vision() );
+        addBehaviour( new TomaDecision() );
+        addBehaviour( new Mover() );
     }
+    
+    
 }
